b9dee7d667b6f208aebf02db198f73ad0c194095
vochithanh89 <61690947+vochithanh89@users.noreply.github.com>
2021-10-18 10:22:15 +0700

    first

---
+++ script/script.js
@@ -0,0 +1,462 @@
+let data = [
+    {
+        id: 0,
+        singer: "Masew, Huyền Tâm Môn",
+        song: "Phố Đã Lên Đèn",
+        ["img-cd"]: "https://photo-resize-zmp3.zadn.vn/w240_r1x1_jpeg/cover/9/7/8/d/978d12830c18df95c26e93e658019166.jpg",
+        ["audio-scr"]: "https://mp3-s1-m-zmp3.zadn.vn/33dbfdebe3aa0af453bb/4457018915500166957?authen=exp=1634549007~acl=/33dbfdebe3aa0af453bb/*~hmac=8d9e9c61d0c10620898c977fc918ab21&fs=MTYzNDM3NjIwNzk5MXx3ZWJWNHw4LjIxLjExLjEyMA"
+    },
+
+    {
+        id: 1,
+        singer: "JSOL",
+        song: "Giá Như Em Nhìn Lại",
+        ["img-cd"]: "https://photo-resize-zmp3.zadn.vn/w240_r1x1_jpeg/cover/3/f/0/a/3f0a8d23baee0c632d2a06221726cc53.jpg",
+        ["audio-scr"]: "https://mp3-s1-m-zmp3.zadn.vn/a406e6efc2a82bf672b9/2218406033048065497?authen=exp=1634551300~acl=/a406e6efc2a82bf672b9/*~hmac=08a8d8334c8a98193d84ca7c0c41e860&fs=MTYzNDM3ODUwMDA3NXx3ZWJWNHw4LjIxLjExLjEyMA"
+    },
+
+    {
+        id: 2,
+        singer: "MONSTAR",
+        song: "có hẹn với thanh xuân",
+        ["img-cd"]: "https://photo-resize-zmp3.zadn.vn/w240_r1x1_jpeg/cover/e/2/3/f/e23ff2faaa64eebfc57e0acde247f0db.jpg",
+        ["audio-scr"]: "https://mp3-s1-m-zmp3.zadn.vn/67bdbe2c376ade34877b/7120856401733841793?authen=exp=1634370274~acl=/67bdbe2c376ade34877b/*~hmac=58cd618faf088a3d7cdad122c294533e&fs=MTYzNDE5NzQ3NDIxOHx3ZWJWNHw4LjIxLjExLjEyMA"
+    },
+
+    {
+        id: 3,
+        singer: "Da LAB",
+        song: "Từ Ngày Em Đến",
+        ["img-cd"]: "https://i.ytimg.com/vi/AmvA-XJF0j8/maxresdefault.jpg",
+        ["audio-scr"]: "https://mp3-s1-m-zmp3.zadn.vn/efad11d2c99620c87987/2245834307933820373?authen=exp=1634370502~acl=/efad11d2c99620c87987/*~hmac=d27a974cdbe9c6af87898c32ad0e6b2b&fs=MTYzNDE5NzmUsICwMjUzN3x3ZWJWNHw4LjIxLjExLjEyMA"
+    },
+
+    {
+        id: 4,
+        singer: "Soobin Hoàng Sơn",
+        song: "Xin Đừng Lặng Im",
+        ["img-cd"]: "https://photo-resize-zmp3.zadn.vn/w240_r1x1_png/covers/1/e/1ebc5f3387b2179e25e55d6208b16b04_1500952255.png",
+        ["audio-scr"]: "https://mp3-s1-m-zmp3.zadn.vn/3cedeac33387dad98396/8127820420268223686?authen=exp=1634550837~acl=/3cedeac33387dad98396/*~hmac=4e22c0f901ce834b384940ea73cfaad7&fs=MTYzNDM3ODAzNzMxMnx3ZWJWNHw4LjIxLjExLjEyMA"
+    },
+
+    {
+        id: 5,
+        singer: "Soobin Hoàng Sơn",
+        song: "Nếu Ngày Ấy",
+        ["img-cd"]: "https://photo-resize-zmp3.zadn.vn/w240_r1x1_jpeg/cover/2/9/b/f/29bf1c15a2aeacf72a2fa1e84d92f3e9.jpg",
+        ["audio-scr"]: "https://mp3-s1-m-zmp3.zadn.vn/659ad76efc2915774c38/9154281717259436772?authen=exp=1634551028~acl=/659ad76efc2915774c38/*~hmac=13e5839c8bfe954889496220f8991737&fs=MTYzNDM3ODIyODUzNXx3ZWJWNHw4LjIxLjExLjEyMA"
+    },
+
+    {
+        id: 6,
+        singer: "Hoàng Dũng",
+        song: "Tôi Là Ai Trong Em",
+        ["img-cd"]: "https://data.chiasenhac.com/data/cover/137/136512.jpg",
+        ["audio-scr"]: "https://mp3-s1-m-zmp3.zadn.vn/68eda39901dde883b1cc/6409415337736237136?authen=exp=1634551112~acl=/68eda39901dde883b1cc/*~hmac=ad3cf94eb8623e8b084b370ee9bfcb32&fs=MTYzNDM3ODMxMjgyOXx3ZWJWNHw4LjIxLjExLjEyMA"
+    },
+
+    {
+        id: 7,
+        singer: "Hoàng Dũng",
+        song: "Đôi Lời",
+        ["img-cd"]: "https://i.ytimg.com/vi/pm-X_mVxQDo/maxresdefault.jpg",
+        ["audio-scr"]: "https://mp3-s1-m-zmp3.zadn.vn/999abd116b55820bdb44/3280504621895968928?authen=exp=1634369933~acl=/999abd116b55820bdb44/*~hmac=80e353a99a8823cdff38499957dded07&fs=MTYzNDE5NzEzMzM5NXx3ZWJWNHw4LjIxLjExLjEyMA"
+    },
+
+    {
+        id: 8,
+        singer: "Thế Bảo",
+        song: "Về Phía Mưa",
+        ["img-cd"]: "https://photo-resize-zmp3.zadn.vn/w240_r1x1_jpeg/covers/3/f/3fb2db6cccf4a23383383394b28b2b31_1509416446.jpg",
+        ["audio-scr"]: "https://mp3-s1-m-zmp3.zadn.vn/5c8008b2d3f63aa863e7/5103899201367301689?authen=exp=1634551514~acl=/5c8008b2d3f63aa863e7/*~hmac=16cc9953bf252315797d2ead928ef2d2&fs=MTYzNDM3ODmUsICxNDEwMHx3ZWJWNHw4LjIxLjExLjEyMA"
+    },
+
+    {
+        id: 9,
+        singer: "Phương Ly",
+        song: "Anh Là Ai",
+        ["img-cd"]: "https://photo-resize-zmp3.zadn.vn/w240_r1x1_jpeg/cover/0/8/0/6/0806ea1c463042079f3fc0316d2032b0.jpg",
+        ["audio-scr"]: "https://mp3-s1-m-zmp3.zadn.vn/66f306f43fb3d6ed8fa2/2631979061389853487?authen=exp=1634551586~acl=/66f306f43fb3d6ed8fa2/*~hmac=39ebaf6fe75aa1fcc96c470210bfd2a8&fs=MTYzNDM3ODmUsIC4Njk2M3x3ZWJWNHw4LjIxLjExLjEyMA"
+    },
+
+    {
+        id: 10,
+        singer: "Phương Ly, JustaTee",
+        song: "Mặt Trời Của Em",
+        ["img-cd"]: "https://photo-resize-zmp3.zadn.vn/w240_r1x1_jpeg/covers/1/9/19c8d9340b18111044e084d806335fd9_1509176983.jpg",
+        ["audio-scr"]: "https://mp3-s1-m-zmp3.zadn.vn/64d9e2e639a2d0fc89b3/5367828857673443509?authen=exp=1634551761~acl=/64d9e2e639a2d0fc89b3/*~hmac=a8ebd7b7b6e583b151206b4ff9a9ff6d&fs=MTYzNDM3ODk2MTUxMHx3ZWJWNHw4LjIxLjExLjEyMA"
+    },
+
+    {
+        id: 11,
+        singer: "JustaTee, Phương Ly",
+        song: "Thằng Điên",
+        ["img-cd"]: "https://photo-resize-zmp3.zadn.vn/w240_r1x1_jpeg/cover/9/d/5/c/9d5c56a277a06a48ec7956a4fd17e4c1.jpg",
+        ["audio-scr"]: "https://mp3-s1-m-zmp3.zadn.vn/6a7f4f5c9c1875462c09/6915996016203233196?authen=exp=1634551826~acl=/6a7f4f5c9c1875462c09/*~hmac=42c0a2d73c4618bebca956024e82d1bf&fs=MTYzNDM3OTAyNjmUsICzM3x3ZWJWNHw4LjIxLjExLjEyMA"
+    },
+]
+
+let $ = document.querySelector.bind(document);
+let $$ = document.querySelectorAll.bind(document);
+let audio = $('.audio');
+let pausePlayBtn = $('.pause-play-btn');
+let cd = $('.cd');
+
+let app = $('.app-container');
+
+let progress = $('.progress');
+let fill = $('.fill');
+let thumb = $('.thumb');
+
+let loopBtn = $('.loop-btn');
+let shuffleBtn = $('.shuffle-btn');
+let statusBtn = $('.status-btn');
+
+let isRandom = false;
+
+
+
+let cdAnimation = cd.animate([
+    {
+        transform: 'rotate(0)'
+    },
+    {
+        transform: 'rotate(359deg)'
+    }
+],
+    {
+        duration: 10000,
+        iterations: Infinity 
+    }
+);
+
+cdAnimation.pause();
+audio.volume = 0.2;
+
+start();
+
+function start() {
+    renderPlaylist(data);
+    loadMusicToPlayer(data[0]);
+    scrollPlaylist();
+    playPauseMusic();
+    clickProgress();
+    dragAndDropProgress();
+    statusMusic();
+    nextMusicBtn();
+    previousMusicBtn();
+    selectMusic();
+}
+
+function renderPlaylist(data) {
+    let playList = $('.playlist');
+    let htmls = data.map(element => {
+        return `
+            <div class="playlist-item" id-audio="${element.id}">
+                <p class="song-number">${element.id + 1}</p>
+                <div class="cd-item" style="background-image: url('${element["img-cd"]}')">
+                    <div class="cd-animation"></div>
+                </div>
+                <div class="title-item">
+                    <p class="song-item">${element.song}</p>
+                    <p class="singer-item">${element.singer}</p>
+                </div>
+            </div>
+        `   
+    });
+    playList.innerHTML = htmls.join('');
+}
+
+
+function loadMusicToPlayer(musicData) {
+    $('.cd-thumb').style.backgroundImage = `url(${musicData["img-cd"]})`;
+    $('.song').innerText = musicData.song;
+    $('.singer').innerText = musicData.singer;
+    audio.src = musicData["audio-scr"];
+    audio.setAttribute('id-audio', musicData.id);
+    audio.addEventListener('loadedmetadata', function() {
+        $('.time-song').innerText = secondToTime(audio.duration);
+    });
+    loadSongWave(musicData.id);
+}
+
+function loadSongWave(id) {
+    let playListItems = $$('.playlist-item');
+    if ($('.playlist-item.active')) {
+        $('.playlist-item.active').classList.remove('active');
+    }
+    playListItems.forEach(function(playListItem) {
+        if (playListItem.getAttribute('id-audio') == id) {
+            playListItem.classList.add('active');
+        }
+    });
+}
+
+function scrollPlaylist() {
+    let scrollTop;
+    let appContainer = $('.app-container');
+    let cd = $('.cd');
+    let cdWidth = cd.offsetWidth;
+    //Mouse
+    appContainer.onscroll = (e) => {
+        scrollTop = cdWidth - e.srcElement.scrollTop;
+        if (scrollTop < 0) {
+            scrollTop = 0;
+        }
+        else if (scrollTop > cdWidth) {
+            scrollTop = cdWidth;
+        }
+        cd.style.width = scrollTop + 'px';
+        cd.style.opacity = scrollTop / cdWidth;
+    }
+    //Touch
+    document.onscroll = (e) => {
+        scrollTop = cdWidth - e.srcElement.scrollingElement.scrollTop;
+        if (scrollTop < 0) {
+            scrollTop = 0;
+        }
+        else if (scrollTop > cdWidth) {
+            scrollTop = cdWidth;
+        }
+        cd.style.width = scrollTop + 'px';
+        cd.style.opacity = scrollTop / cdWidth;
+    }
+}
+
+function secondToTime(second) {
+    second = Math.round(second);
+    return new Date(second * 1000).toISOString().substr(14, 5);
+}
+
+function playPauseMusic() {
+    let isPlaying = false;
+    audio.onplaying = () => {
+        isPlaying = true;
+    }
+    audio.onpause = () => {
+        isPlaying = false;
+    }
+    pausePlayBtn.onclick = function() {
+        isPlaying = !isPlaying;
+        if(isPlaying) {
+            playMusic();
+        }
+        else {
+            pauseMusic();
+        }
+    }
+}
+
+function updateProgress() {
+    audio.addEventListener('timeupdate', () => {
+        $('.time-progress').innerText = secondToTime(audio.currentTime);
+        fill.style.width = 100 / audio.duration * audio.currentTime + '%';
+        thumb.style.left = 100 / audio.duration * audio.currentTime + '%';
+        if (audio.ended) {
+            pauseMusic();
+            checkStatus(statusBtn.getAttribute('status'));
+        }
+    });
+}
+
+function clickProgress() {
+    progress.onclick = e => {
+        let percent = 100 / progress.offsetWidth * e.offsetX;
+        fill.style.width = percent + '%';
+        thumb.style.left = percent + '%';       
+        audio.currentTime = audio.duration / 100 * percent;
+    } 
+}
+
+function dragAndDropProgress() {
+    let dragging = false;
+    //Mouse event
+    thumb.onmousedown = e => {
+        dragging = true;
+    }
+    document.onmousemove = e => {
+        if (dragging) {
+            let appLeft = app.offsetLeft + 16;
+            let percent = 100 / progress.offsetWidth * (e.clientX - appLeft);
+            if (percent < 0) {
+                percent = 0;
+            }
+            if(percent > 100) {
+                    percent = 100;
+                }
+                fill.style.width = percent + '%';
+                thumb.style.left = percent + '%';
+                audio.currentTime = audio.duration / 100 * percent;
+            }
+        }
+        document.onmouseup = e => {
+            dragging = false;
+        }
+    // Touch Event    
+    document.ontouchmove = function(e) {
+        if (dragging) {
+            let appLeft = app.offsetLeft + 16;
+            let percent = 100 / progress.offsetWidth * (e.touches[0].clientX - appLeft);
+            if (percent < 0) {
+                percent = 0;
+            }
+            if(percent > 100) {
+                percent = 100;
+            }
+            fill.style.width = percent + '%';
+            thumb.style.left = percent + '%';
+            thumb.style['box-shadow'] = '0 0 0 10px rgba(100, 148, 237, 0.2)';
+            audio.currentTime = audio.duration / 100 * percent;
+        }
+    }
+
+    thumb.ontouchstart = function() {
+        dragging = true;
+    }
+    
+    document.ontouchend = function() {
+        thumb.style['box-shadow'] = '0 0 0 6px rgba(100, 148, 237, 0.2)';
+        dragging = false;
+    }
+
+}
+
+
+function playMusic() {
+    pausePlayBtn.classList.remove('pause');
+    pausePlayBtn.classList.add('play');
+    audio.play();
+    updateProgress();
+    cdAnimation.play();
+    let playingItem = $('.playlist-item.active .cd-animation');
+    playingItem.style.backgroundImage = "url('../img/icon-playing.gif')"
+}
+
+function pauseMusic() {
+    pausePlayBtn.classList.remove('play');
+    pausePlayBtn.classList.add('pause');
+    audio.pause();
+    cdAnimation.pause();
+    let playingItem = $('.playlist-item.active .cd-animation');
+    playingItem.style.backgroundImage = "url('../img/images.png')"
+}
+
+
+function nextMusicBtn() {
+    let nextBtn = $('.next-btn');
+    nextBtn.onclick = () => {
+        let id;
+        if (isRandom) {
+            id = randomId();
+        }
+        else {
+            id = Number(audio.getAttribute('id-audio')) + 1;
+        }
+        nextMusic(id);
+    };
+}
+
+function nextMusic(id) {
+    if (id < data.length) {
+        loadMusicToPlayer(data[id]);
+        playMusic();
+    }
+}
+
+function previousMusicBtn() {
+    let nextBtn = $('.previous-btn');
+    nextBtn.onclick = () => {
+        let id;
+        if (isRandom) {
+            id = randomId();
+        }
+        else {
+            id = Number(audio.getAttribute('id-audio')) - 1;
+        }
+        previousMusic(id);
+    };
+}
+
+function previousMusic(id) {
+    if (id >= 0) {
+        loadMusicToPlayer(data[id]);
+        playMusic();
+    }
+}
+
+function statusMusic() {
+    statusBtn.onclick = function() {
+        let status = $('.status-btn').getAttribute('status');
+        switch (status) {
+            case 'no-loop':
+                statusBtn.setAttribute('status', 'loop');
+                loopBtn.style.display = 'initial';
+                loopBtn.style.opacity = '1';
+                shuffleBtn.style.display = 'none';
+                isRandom = false;
+                break;
+            case 'loop':
+                statusBtn.setAttribute('status', 'shuffle');
+                loopBtn.style.display = 'none';
+                shuffleBtn.style.display = 'initial';
+                isRandom = true;
+                break;
+            case 'shuffle':
+                statusBtn.setAttribute('status', 'no-loop');
+                loopBtn.style.display = 'initial';
+                loopBtn.style.opacity = '0.6';
+                shuffleBtn.style.display = 'none';
+                isRandom = false;
+                break;
+        }
+    }
+}
+
+function checkStatus(status) {
+    switch (status) {
+        case 'no-loop':
+            noLoopMusic();
+            break;
+        case 'loop':
+            loopMusic();
+            break;
+        case 'shuffle':
+            shuffleMusic();
+            break;    
+    }
+}
+
+function loopMusic() {
+    playMusic();
+}
+
+function shuffleMusic() {
+    nextMusic(randomId());
+}
+
+function randomId() {
+    let randomId;
+    let idAudio = audio.getAttribute('id-audio');
+    do {
+        randomId = Math.floor(Math.random()*data.length);
+    }
+    while (randomId == idAudio);
+    return randomId;
+}
+
+function noLoopMusic() {
+    let id = Number(audio.getAttribute('id-audio')) + 1;
+    nextMusic(id);
+}
+
+
+function selectMusic() {
+    let playListItems = $$('.playlist-item');
+    playListItems.forEach(function(playListItem) {
+        playListItem.onclick = () => {
+            let id = playListItem.getAttribute('id-audio');
+            loadMusicToPlayer(data[id]);
+            playMusic();
+        }
+    })
+}
